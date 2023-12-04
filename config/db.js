50adbb40c1b3c103d88fa6f82d2f43f6893602e6
Amin <amin19.kntu@gmail.com>
2023-12-04 23:19:19 +0100

    in this part app is connected to the mongodb server

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose') ;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      
+    });
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+
+  }
+};
+
+mongoose.set('strictQuery', true);
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const express=require('express');
-const port=5001;
+const express = require('express');
+require('dotenv').config();
+const port = process.env.PORT || 5001;
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
 
 // Body parser middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended:false }));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.json({ message:'Wellcome to the RandomIdea API'});
+  res.json({ message: 'Welcome to the RandomIdeas API' });
+});
 
-})
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter);
 
-const ideasRouter= require('./routes/ideas');
-app.use('/api/ideas', ideasRouter)
-
-app.listen(port, () => { 
-    console.log(`Server listening on port ${port}`);
- });
+app.listen(port, () => console.log(`Server listening on port ${port}`));
